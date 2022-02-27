e4e26c545cc0cc980edd6971ca3b7681f80cff2f
Matthew Stead <mattstead6@gmail.com>
2022-02-26 22:33:56 -0500

    commit tree 0c3ec026cf1338a56266d05618b22065351ae4dd

--- index.js
+++ index.js
@@ -2,27 +2,156 @@
 
 const employee = {
     name: "Matt",
-    streetAddress: "200 Smith St"
+    streetAddress: "38 Victoia Road"
 }
 
-function updateEmployeeWithKeyAndValue (employee, key, value){
-    const newObj = {...employee};
-    newObj[key] = value;
-    return newObj;
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value  }
+    
+    }  
+
+
+// function updateEmployeeWithKeyAndValue(obj, key, value){
+//     const newObj = {...obj, [key]: value}
+//     return newObj;
+// }
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+     obj[key] = value
+     return employee
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value){
-    employee [key] = value;
-    return employee;
-}
 
-function deleteFromEmployeeByKey(employee, key){
-    const newEmployee = {...employee};
+
+
+function deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj}
     delete newEmployee[key];
-    return newEmployee;
+return newEmployee
 }
+    
 
-function destructivelyDeleteFromEmployeeByKey (employee, key){
-    delete employee[key];
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
     return employee;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const employee = {
+//     name: "Matt",
+//     streetAddress: "38 Victoria Road",
+// }
+
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//     const newObj = {...employee, [key]:value}
+//     return newObj;
+// }
+
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//     employee [key] = value;
+//     return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, key){
+//     const newEmployee = {...employee}
+//     delete newEmployee [key];
+//     return newEmployee;
+// }
+
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key){
+//     delete employee[key];
+//     return employee;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const employee = {
+//     name: "Matt",
+//     streetAddress: "200 Smith St"
+// }
+
+// function updateEmployeeWithKeyAndValue (employee, key, value){
+//     const newObj = {...employee};
+//     newObj[key] = value;
+//     return newObj;
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value){
+//     employee [key] = value;
+//     return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, key){
+//     const newEmployee = {...employee};
+//     delete newEmployee[key];
+//     return newEmployee;
+// }
+
+// function destructivelyDeleteFromEmployeeByKey (employee, key){
+//     delete employee[key];
+//     return employee;
+// }
